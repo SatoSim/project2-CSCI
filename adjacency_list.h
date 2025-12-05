41366006e321b9d972e822503243bf34fd4b53b0
Satoshi Simental <siment1@stolaf.edu>
2025-12-05 14:13:05 -0600

    all

---
+++ adjacency_list.h
@@ -0,0 +1,56 @@
+#ifndef ADJACENCY_LIST
+#define ADJACENCY_LIST
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+
+class AdjacencyList {
+private:
+    vector<vector<int> > adjList;
+    bool isDirected;
+
+public:
+    AdjacencyList(int vertices, bool directed = false)
+    {
+        adjList.resize(vertices);
+        isDirected = directed;
+    }
+
+    void addEdge(int src, int dest)
+    {
+        adjList[src].push_back(dest);
+        if (!isDirected) {
+            adjList[dest].push_back(src);
+        }
+    }
+
+	int* getN(int v){
+
+		
+		int* neigh = new int[adjList[v].size()];
+		for (int j = 0; j < adjList[v].size(); ++j) {
+                neigh[j] = adjList[v][j];
+            }
+		for (int j = 0; j < adjList[v].size(); ++j) {
+                cout << neigh[j] << ", ";
+            }
+		return neigh;
+	}
+
+    void printList()
+    {
+        for (int i = 0; i < adjList.size(); ++i) {
+            cout << i << ": ";
+            for (int j = 0; j < adjList[i].size(); ++j) {
+                cout << adjList[i][j] << ",";
+            }
+            cout << "NULL" << endl;
+        }
+    }
+};
+
+
+#endif
--- testing.cpp
+++ testing.cpp
@@ -1,3 +1,53 @@
 #include <iostream>
-#include "bfs_compute_distances.h"
+//#include "bfs_compute_distances.h"
+#include "adjacency_list.h"
 using namespace std;
+
+
+void test_helper( ){
+    // input is an adjacency matrix 
+
+
+    
+}
+
+void test_short_distance() {
+ 
+    bool status = true; // change status if testing goes wrong 
+
+    // adjacency matrices 
+    // 5 examples 
+    int vertices = 3;
+    AdjacencyList list(vertices);
+    
+
+    // Example 1 
+    // creating adjacency list 
+
+    
+    list.addEdge(0, 1);
+    list.addEdge(0, 2);
+    list.printList();
+
+    list.getN(0);
+	
+
+
+
+
+    //status = test_helper(matrix_num, correct_answer);
+
+
+    if(status){
+        cout<<"Everything is correct wohoooo!";
+    }else{
+        cout<<"Get back to think and code!";
+    }
+    
+
+}
+
+int main() {
+    test_short_distance();
+    return 0;
+}
